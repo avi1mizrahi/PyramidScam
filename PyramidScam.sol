e2cfe3eb71a53af17d6abb79f49247b3d7eae0fe
roibh86 <roi_b_h@hotmail.com>
2018-12-30 23:17:34 +0200

    Update PyramidScam.sol

--- PyramidScam.sol
+++ PyramidScam.sol
@@ -2,18 +2,66 @@ pragma solidity >=0.4.22 <0.6.0;
 
 
 contract PyramidScam {
-    uint constant public joiningFee = 1 ether;
+    uint constant public joiningFee;
     address private owner;
+    uint private availableTokens;
+    uint constant public tokenProducerPrice;
+    uint constant public tokenConsumerPrice;
     mapping (address => address) private affiliates;
-    mapping (address => uint) pendingWithdrawals;
+    mapping (address => uint)    pendingWithdrawals;
+    mapping (address => uint)    pendingTokens;
     uint nPending = 0;
 
     address constant Null = address(0);
-
-    constructor() public {
-        owner = msg.sender;
+    
+    
+    modifier ifOwner(){
+        if (owner != msg.sendr) {
+           throw;
+        }
+        else{
+           _;
+        }
+    }
+      
+    
+    constructor(uint _joiningFee, uint _availableTokens, uint _tokenConsumerPrice, uint _tokenSupplierPrice) public {
+        owner              = msg.sender;
+        joiningFee         = _joiningFee;
+        availableTokens    = _availableTokens;
+        tokenConsumerPrice = _tokenConsumerPrice;
+        tokenSupplierPrice = _tokenSupplierPrice;
     }
 
+    function addTokens() ifOwner private payable {
+        availableTokens += msg.value;
+    }
+
+    function buyTokensSupplier() public payable {
+        uint payment;
+        if (affiliates[msg.sender] != NULL) {
+            payment = msg.value * tokenSupplierPrice;
+            if (payment <= pendingWithdrawals[msg.sender]) {
+                pendingTokens[msg.sender]      += msg.value;
+                pendingWithdrawals[msg.sender] -= payment;
+                pendingTokens[owner]           -= msg.value;
+                pendingWithdrawals[owner]      += payment;
+            }
+        }
+    }
+    
+     function buyTokensConsumer(address supplier) public payable returns (uint){
+        uint tokenAmount = msg.value / tokenConsumerPrice;
+        if (affiliates[supplier] != NULL) {
+            if (tokenAmount <= pendingTokens[supplier]) {
+                pendingTokens[supplier]        -= tokenAmount;
+                pendingWithdrawals[supplier]   += msg.value;
+                return tokenAmount;
+            }
+        }
+        return 0;
+    }
+    
     function join(address referral) public payable {
         if (msg.value < joiningFee) return;
         if (affiliates[msg.sender] != Null || msg.sender == owner) return; // TODO: do better here
@@ -22,7 +70,7 @@ contract PyramidScam {
 
         affiliates[msg.sender] = referral;
 
-        uint amount = msg.value;
+        uint amount = joiningFee;
         while(referral != Null) {
             amount /= 2;
             if (pendingWithdrawals[referral] == 0 && amount > 0)
@@ -30,6 +78,10 @@ contract PyramidScam {
             pendingWithdrawals[referral] += amount;
             referral = affiliates[referral];
         }
+        
+        uint initialDeposit = msg.value - joiningFee;
+        uint initialTokens  = initialDeposit/tokenSupplierPrice;
+        pendingTokens[msg.sender] = initialTokens;
     }
 
     function withdraw() public {
